97fe3fd39ad85c54cd40c49e24bad32450523039
Nikolay Yusev <nyusev@ptsecurity.com>
2016-07-15 15:07:57 +0700

    add git_clone.py

---
+++ git_clone.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Script to clone all the github repos that a user is watching
+
+import subprocess
+
+print("Start!")
+print("----------------------------------")
+
+subprocess.call('git pull --no-edit https://github.com/nikolasj/Lab_C.git feature', shell=True)
+subprocess.call('touch test.txt', shell=True)
+subprocess.call('git diff master^ >> test.txt', shell=True)
+
+print("----------------------------------")
+print("Finish!")
+
+
+
+# subprocess.call('echo "start"', shell=True)
+# subprocess.call('mkdir /home/cm/test_git/', shell=True)
+# subprocess.call('cd /home/cm/test_git/; echo "one" | git clone https://github.com/nikolasj/Lab_C.git; echo "two"', shell=True)
+# subprocess.call('git clone https://github.com/nikolasj/Lab_C.git', shell=True)
+# subprocess.call('echo "finish"', shell=True)
